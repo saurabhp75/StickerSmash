08ca8e2bdf706e9669f0e829a2733eb6a97df7d2
Saurabh Prakash <saurabhp75@gmail.com>
2025-06-02 06:55:17 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    marginTop: 16,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#1e90ff",
+    borderRadius: 5,
+  },
+});
